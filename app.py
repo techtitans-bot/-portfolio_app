df9d65cd80d7a1ad85e3a9955d7939f0611fdd83
Tansdevillian <markadijado@gmail.com>
2026-05-04 12:38:44 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mark Gracio", page_icon="🎨", layout="wide")
+
+st.title("🎨 Mark Gracio Portfolio")
+st.markdown("**3rd Year CS | Creative Designer | Award Winner**")
+
+st.sidebar.success("📱 Contact Me!")
