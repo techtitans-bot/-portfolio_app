5315d0e059b9aa82a5c51ca6f8cac3e82f563d67
Tansdevillian <markadijado@gmail.com>
2026-05-04 12:40:16 +0800

    Add home page with welcome message and details

---
+++ pages/1_Home.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header("🏠 Welcome!")
+st.write("Mark Gracio - 21 years old, 3rd year CS Student")
+st.write("Graphic Designer • Event Decoration • Programmer • Video & Photo Editor")
