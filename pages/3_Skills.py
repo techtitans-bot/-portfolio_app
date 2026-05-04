69ad5065a1038077c0765ad6ec4e149d645c0b61
Tansdevillian <markadijado@gmail.com>
2026-05-04 12:43:14 +0800

    Add skills display using Streamlit

---
+++ pages/3_Skills.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.header("🎨 My Skills")
+st.write("**Design-editing:** Canva, PicsArt, Adobe, Capcut")
+st.write("**Tech:** Python, Tech Assistant, Streamlit")
+st.write("**Events:** Table Skirting, Decoration, Make-up, Catering Services")
